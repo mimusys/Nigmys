d2cfa1d0b245137266b8b4e2a29f24c10f58b794
ereveresmith <ereveresmith@gmail.com>
2015-11-24 21:44:31 -0700

    We set up the basics for passing data, comparing usernames, and passwords.

--- Homer_MVC/Controllers/LoginController.cs
+++ Homer_MVC/Controllers/LoginController.cs
@@ -19,13 +19,45 @@ namespace Homer_MVC.Controllers
 
         public ActionResult Index()
         {
+            System.Diagnostics.Debug.WriteLine("Loading page");
             return View();
         }
 
         [HttpPost]
         public ActionResult CheckLogin(string username, string password)
         {
-            return Json(new {Message = userSql.GetConnString(), Username = username, Password = password });
+            System.Diagnostics.Debug.WriteLine("Checking Login");
+
+            
+            List<string> userNameList = userSql.getUserNames();
+
+            if(userNameList == null)
+            {
+                System.Diagnostics.Debug.WriteLine("No users in the list!");
+            }
+
+            if (userNameList.Contains(username))
+            {
+                System.Diagnostics.Debug.WriteLine("Login was Successful!");
+                String[] dbPass = userSql.getPasswordInfo(username);
+                String salt = dbPass[1];
+                String hash = dbPass[0];
+
+
+                //Figure out the salt/hash system and compare it to the 'password' parameter
+                //If password == password in db,
+                    //Redirect to Dashboard  
+                    //@Url.Action("Index", "SignUp")
+
+
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Login was NOT Successful!");
+            }
+
+            return Json(new { Message = userSql.GetConnString(), Username = username, Password = password });
+
         }
 
     }
