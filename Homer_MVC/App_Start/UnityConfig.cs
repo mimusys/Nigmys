8e2b7135b0d654e8ff634a4a70055a1165c2ea34
mimusys <mimusys@gmail.com>
2015-12-07 21:32:05 -0700

    Database MySql return with medium trust level

--- Homer_MVC/App_Start/UnityConfig.cs
+++ Homer_MVC/App_Start/UnityConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using Microsoft.Practices.Unity;
-using System.Data.Odbc;
+using MySql.Data.MySqlClient;
 using System.Collections.Specialized;
 using System.Web.Mvc;
 using Nigmys.Support;
@@ -14,8 +14,6 @@ namespace Homer_MVC.App_Start
 {
     public class UnityConfig
     {
-        static String driver = "Driver={MySQL ODBC 5.3 Unicode Driver};";
-        static String option = "Option=3;";
         public static void ConfigureDependencies()
         {
             IUnityContainer container = new UnityContainer();
@@ -27,11 +25,11 @@ namespace Homer_MVC.App_Start
         private static void ConfigureUserSQL(IUnityContainer container)
         {
             Configurations.SQLConfig config = (Configurations.SQLConfig)System.Configuration.ConfigurationManager.GetSection("sqlConfigurations/mySQLUserConfig");
-            String connString = driver+"Server=" + config.Database.Address + ";User=" + config.Database.Username + ";Password=" + config.Database.Password 
-                + ";Database=" + config.Database.Name + ";"+option;
+            String connString = "Server=" + config.Database.Address + ";User=" + config.Database.Username + ";Password=" + config.Database.Password 
+                + ";Database=" + config.Database.Name + ";";
             
-            container.RegisterType<OdbcConnection>("UserConnect", new InjectionConstructor(connString));
-            container.RegisterType<ISqlUserDatabase, SqlUserDatabase>("UserDB", new InjectionConstructor(new ResolvedParameter<OdbcConnection>("UserConnect")));
+            container.RegisterType<MySqlConnection>("UserConnect", new InjectionConstructor(connString));
+            container.RegisterType<ISqlUserDatabase, SqlUserDatabase>("UserDB", new InjectionConstructor(new ResolvedParameter<MySqlConnection>("UserConnect")));
             container.RegisterType<IController, LoginController>(new InjectionConstructor(new ResolvedParameter<SqlUserDatabase>("UserDB")));
         }
     }
