923d2334e1c98a78e5f7287bcf0d1d013baf98b0
abrady3 <a.brady.3@gmail.com>
2015-12-17 13:50:34 -0700

    Add GetUsername action to LandingPage

--- Homer_MVC/Controllers/LandingPageController.cs
+++ Homer_MVC/Controllers/LandingPageController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Homer_MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,5 +15,13 @@ namespace Homer_MVC.Controllers
             return View();
         }
 
+        public String GetUsername() {
+            User user = (User)Session["user"];
+            if (user != null) {
+                return user.Username;
+            } else {
+                return "Login/Signup";
+            }
+        }
     }
 }
