fd6638709d3614c4a5c7ff1e80816251c4402947
abrady3 <a.brady.3@gmail.com>
2016-01-07 23:57:03 -0700

    Add Portfolio DB logic

---
+++ Homer_MVC/Services/ISqlPortfolioDatabase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homer_MVC {
+    public interface ISqlPortfolioDatabase : ISqlDatabase {
+
+        int createNewPortfolioID();
+
+        void deletePortfolioID(int id);
+    }
+}
---
+++ Homer_MVC/Services/SqlPortfolioDatabase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+
+namespace Homer_MVC.Services {
+    public class SqlPortfolioDatabase : SqlDatabase, ISqlPortfolioDatabase {
+
+        public SqlPortfolioDatabase(MySqlConnection conn) : base(conn) {
+        }
+
+        public int createNewPortfolioID() {
+            int portfolioID = -1;
+            if (Open()) {
+                MySqlCommand cmd = new MySqlCommand("INSERT INTO portfolio VALUES(null);", conn);
+                cmd.Prepare();
+
+                object portfolioIDObj = cmd.ExecuteScalar();
+                if (portfolioIDObj != null) {
+                    portfolioID = Convert.ToInt32(portfolioIDObj);
+                }
+
+                Close();
+            }
+            return portfolioID;
+        }
+
+        public void deletePortfolioID(int id) {
+            if (Open()) {
+                MySqlCommand cmd = new MySqlCommand("DELETE FROM portfolio WHERE portfolioID = @portfolioID", conn);
+                cmd.Prepare();
+                cmd.Parameters.AddWithValue("@portfolioID", id);
+
+                cmd.ExecuteNonQuery();
+                Close();
+            }
+        }
+    }
+}
