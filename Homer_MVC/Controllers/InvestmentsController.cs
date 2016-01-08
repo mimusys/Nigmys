e0033c518ead6f0d9ce2fd3f9c3b314b7397e672
abrady3 <a.brady.3@gmail.com>
2016-01-07 19:04:49 -0700

    Implement LoggedInFilter which prevents unauthorized access

--- Homer_MVC/Controllers/InvestmentsController.cs
+++ Homer_MVC/Controllers/InvestmentsController.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using Homer_MVC.ActionFilters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
 namespace Homer_MVC.Controllers {
+    [LoggedInFilter]
     public class InvestmentsController : Controller {
 
         public ActionResult AddInvestment() {
