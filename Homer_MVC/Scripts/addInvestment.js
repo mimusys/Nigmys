bed88113f3c8f5795079afe5932c74186b8efae7
abrady3 <a.brady.3@gmail.com>
2016-01-09 21:52:40 -0700

    Fix id clashes in added debt partners

--- Homer_MVC/Scripts/addInvestment.js
+++ Homer_MVC/Scripts/addInvestment.js
@@ -4,27 +4,33 @@
     return (monthlyPercent * loanAmount * Math.pow((1 + monthlyPercent), numPayments)) / (Math.pow(1 + monthlyPercent, numPayments) - 1);
 }
 
+function testDebtPartnerClass() {
+    $('.debt-partner-row').each(function (i, obj) {
+        alert($(this).find('.lenderName').val());
+    });
+}
+
 $(document).ready(function () {
     // add new debt partner list item
     $('a#addDebtPartnerLink').click(function (e) {
         e.preventDefault();
         $('#addDebtPartnerLinkRow').before('\
-<div class="row">\
+<div class="row debt-partner-row">\
     <div class="form-group col-lg-4">\
         <label for="lenderName">Lender Name</label>\
-        <input type="text" value="" id="lenderName" class="form-control" name="lenderName">\
+        <input type="text" value="" class="form-control lenderName">\
     </div>\
     <div class="form-group col-lg-2">\
         <label for="annualPercentageRate">Annual Percent</label>\
-        <input type="text" value="" id="annualPercentageRate" class="form-control" name="annualPercentageRate" placeholder="%0.00">\
+        <input type="text" value="" class="form-control annualPercentageRate" placeholder="%0.00">\
     </div>\
     <div class="form-group col-lg-3">\
         <label for="loanStartDate">Loan Start Date</label>\
-        <input type="date" value="" id="loanStartDate" class="form-control" name="loanStartDate">\
+        <input type="date" value="" class="form-control loanStartDate">\
     </div>\
     <div class="form-group col-lg-2">\
         <label for="termLength">Term Length</label>\
-        <input type="text" valud="" id="termLength" class="form-control" name="termLength" placeholder="Years">\
+        <input type="text" valud="" class="form-control termLength" placeholder="Years">\
     </div>\
     <div class="form-group col-lg-1">\
         <span style="min-height:42px; display:inline-block;"></span>\
@@ -131,6 +137,7 @@ $(document).ready(function () {
     $('.next').click(function () {
         var nextId = $(this).parents('.tab-pane').next().attr("id");
         $('[href=#' + nextId + ']').tab('show');
+        testDebtPartnerClass();
     })
 
     $('.prev').click(function () {
