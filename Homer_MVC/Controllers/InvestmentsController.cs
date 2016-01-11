3b854433236bf3dc5dcbd4253a6a937bc1b4a16a
abrady3 <a.brady.3@gmail.com>
2016-01-10 20:22:17 -0700

    Add AddInvestment action to investment controller

--- Homer_MVC/Controllers/InvestmentsController.cs
+++ Homer_MVC/Controllers/InvestmentsController.cs
@@ -1,4 +1,5 @@
 ﻿using Homer_MVC.ActionFilters;
+using Homer_MVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,26 @@ using System.Web.Mvc;
 namespace Homer_MVC.Controllers {
     [LoggedInFilter]
     public class InvestmentsController : Controller {
+        ISqlInvestmentInformationDatabase investmentDb;
+        ISqlPortfolioDatabase portfolioDb;
+
+        public InvestmentsController(ISqlInvestmentInformationDatabase investmentDb, ISqlPortfolioDatabase portfolioDb) {
+            this.investmentDb = investmentDb;
+            this.portfolioDb = portfolioDb;
+        }
 
         public ActionResult AddInvestment() {
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult AddInvestment(InvestmentInformation investment) {
+            // todo: input validation
+            
+            int newInvestmentID = investmentDb.addNewInvestment(investment);
+            bool success = portfolioDb.addInvestmentID(((User)(Session["user"])).PortfolioID, newInvestmentID);
+
+            return Json(success);
         }
     }
 }
