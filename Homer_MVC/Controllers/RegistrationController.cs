5e1916fc7c83e80be415333f773310fe4d0d8e90
mimusys <mimusys@gmail.com>
2016-06-02 12:22:00 -0700

    Changed Registration folder to hold both sign in and sign up scripts

--- Homer_MVC/Controllers/RegistrationController.cs
+++ Homer_MVC/Controllers/RegistrationController.cs
@@ -11,59 +11,17 @@ using System.Web.Mvc.Html;
 namespace Nigmys.Controllers
 {
     public class RegistrationController : Controller {
-        private ISqlUserDatabase userSql;
-        private ISqlPortfolioDatabase portfolioSql;
 
-        [InjectionConstructor]
-        public RegistrationController(ISqlUserDatabase userSql, ISqlPortfolioDatabase portfolioSql) {
-            this.userSql = userSql;
-            this.portfolioSql = portfolioSql;
-        }
+        public ActionResult SignUp()
+        {
 
-        public ActionResult Index() {
             return View();
         }
 
-        [HttpPost]
-        public JsonResult CheckUsername(String username) {
-            return Json(!userSql.doesUsernameExist(username));
+        public ActionResult SignIn()
+        {
+            return View();
         }
-
-        [HttpPost]
-        public JsonResult CheckEmail(String email) {
-            return Json(!userSql.doesEmailExist(email));
-        }
-
-        [HttpPost]
-        public JsonResult NewUser(User user) {
-            user.PortfolioID = portfolioSql.createNewPortfolioID();
-            int userId = userSql.addNewUser(user);
-            if (userId != -1) {
-                user.UserID = userId.ToString();
-                Session["user"] = user;
-                return Json(true);
-            } else {
-                portfolioSql.deletePortfolioID(user.PortfolioID);
-            }
-            return Json(false);
-        }
-
-        [HttpPost]
-        public JsonResult UploadPicture() {
-            var file = Request.Files[0];
-            if (Session["user"] != null && file != null) {
-                User user = (User)Session["user"];
-                var filename = user.UserID + Path.GetExtension(file.FileName);
-                string urlPath = "~/Images/Profile/" + filename;
-                user.PictureURL = urlPath;
-                Directory.CreateDirectory(Server.MapPath("~/Images/Profile"));
-                var path = Path.Combine(Server.MapPath("~/Images/Profile/"), filename);
-                file.SaveAs(path);
-                if (userSql.setProfileUrl(user.UserID, urlPath)) {
-                    return Json(true);
-                }
-            }
-            return Json(false);
-        }
+        
     }
 }
